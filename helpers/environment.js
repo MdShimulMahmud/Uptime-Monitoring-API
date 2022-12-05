997b21142ca5462fbd06995cc3ef342dd05473b0
Shimul Mahmud <shimulmahmud18@gmail.com>
2022-12-06 00:57:50 +0600

    lesson-6 done

--- helpers/environment.js
+++ helpers/environment.js
@@ -4,12 +4,24 @@ enviromnents.staging = {
   port: 3000,
   envName: "staging",
   secretKey: "jsdfghjsdhfjhdsf",
+  maxChecks: 5,
+  twilio: {
+    fromPhone: "",
+    accountSid: "",
+    authToken: "",
+  },
 };
 
 enviromnents.production = {
   port: 5000,
   envName: "production",
   secretKey: "sjfdhkjshdkjfj",
+  maxChecks: 5,
+  twilio: {
+    fromPhone: "",
+    accountSid: "",
+    authToken: "",
+  },
 };
 
 const currentEnvironment =
--- helpers/utilities.js
+++ helpers/utilities.js
@@ -19,7 +19,7 @@ utilities.parseJSON = (jsonString) => {
 // hash string
 utilities.hash = (str) => {
   if (typeof str === "string" && str.length > 0) {
-    console.log(environments, process.env.NODE_ENV);
+    // console.log(environments, process.env.NODE_ENV);
     const hash = crypto
       .createHmac("sha256", environments.secretKey)
       .update(str)
@@ -29,32 +29,22 @@ utilities.hash = (str) => {
   return false;
 };
 
+utilities.createRandomString = (strlength) => {
+  let length = strlength;
+  length = typeof strlength === "number" && strlength > 0 ? strlength : false;
+
+  if (length) {
+    const possiblecharacters = "abcdefghijklmnopqrstuvwxyz1234567890";
+    let output = "";
+    for (let i = 1; i <= length; i += 1) {
+      const randomCharacter = possiblecharacters.charAt(
+        Math.floor(Math.random() * possiblecharacters.length)
+      );
+      output += randomCharacter;
+    }
+    return output;
+  }
+  return false;
+};
 // export module
 module.exports = utilities;
-
-// const crypto = require("crypto");
-// const environments = require("../helpers/environment");
-// const utilities = {};
-
-// utilities.parseJSON = (jsonString) => {
-//   let output;
-
-//   try {
-//     output = JSON.parse(jsonString);
-//   } catch (error) {
-//     output = {};
-//   }
-//   return output;
-// };
-
-// utilities.hash = (str) => {
-//   if (typeof str === "string" && str.length > 0) {
-//     const hash = crypto
-//       .createHmac("sha256", environments.secretKey)
-//       .update(str)
-//       .digest("hex");
-//     return hash;
-//   }
-//   return false;
-// };
-// module.exports = utilities;
